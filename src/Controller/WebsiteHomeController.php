7f81cca9d4ff7aad7de33e1f7147cc80315ef2a4
Marco L. Abreu <marcolabreu@royalmail.top>
2019-03-06 16:28:35 +0100

    Add homepage

---
+++ src/Controller/WebsiteHomeController.php
@@ -0,0 +1,27 @@
+<?php
+// src/Controller/WebsiteHomeController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WebsiteHomeController
+{
+
+    /**
+     * @Route("/ ")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>
+                <h1>Website-Skeleton</h1>
+                <p>Lucky number: ' . $number . '</p>
+            </body></html>'
+        );
+    }
+}
